3096b230f0c94c2bd6f1c94ef28315f6d78059c8
JNapolitanoIT <jnapolitanoit@gmail.com>
2023-01-18 08:46:31 -0800

    Update tests
    Used PHPCBF to reformat the Connect class

--- src/Connect.php
+++ src/Connect.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace DatabaseFactory {
-
+    
     use PDO;
     use DatabaseFactory\Exceptions;
     use DatabaseFactory\Connections;
-
+    
     /**
      * This is the main connection entry point. This class
      * is responsible for establishing a new PDO connection
@@ -21,22 +21,22 @@ namespace DatabaseFactory {
     {
         /** @var string $username Username */
         private static string $username;
-
+        
         /** @var string $password Password */
         private static string $password;
-
+        
         /** @var string $hostname Hostname */
         private static string $hostname;
-
+        
         /** @var string $database Database */
         private static string $database;
-
+        
         /** @var \PDO $connection PDO instance */
         private static PDO $connection;
-
+        
         /** @var string $driver Database driver */
         private static string $driver;
-
+        
         /**
          * PDO connection instance
          *
@@ -51,27 +51,34 @@ namespace DatabaseFactory {
                 self::$username = getenv('DB_USERNAME');
                 self::$password = getenv('DB_PASSWORD');
                 self::$hostname = getenv('DB_HOSTNAME');
-
-                // credentials check
-                if (!self::$username || !self::$password) {
-                    throw new Exceptions\InvalidCredentialsException();
-                }
-
+                
                 // set driver
                 self::setDriver(getenv('DB_DRIVER'));
-
-                // driver check
-                if (!self::$driver) {
-                    throw new Exceptions\InvalidDriverException();
+                
+                // in the event that we are not connecting
+                // to a SQLite3 database, let's validate a
+                // users credentials
+                if (!self::$driver === 'sqlite') {
+                    if (!self::$username || !self::$password) {
+                        throw new Exceptions\InvalidCredentialsException();
+                    }
                 }
-
+                
                 // connection implementation
                 switch (self::$driver) {
                     // MySQL / MariaDB / Default
                     case 'mysql':
                     case 'maria':
                     default:
-                        self::newConnection(Connections\MySQL::connection(self::$database, self::$hostname));
+                        self::newConnection(
+                            Connections\MySQL::connection(self::$database, self::$hostname)
+                        );
+                        break;
+                    // SQLite 3
+                    case 'sqlite':
+                        self::newConnection(
+                            Connections\SQLite::connection(self::$database, self::$hostname)
+                        );
                 }
                 // return the connection
                 return self::$connection;
@@ -80,7 +87,7 @@ namespace DatabaseFactory {
                 throw new Exceptions\ConnectionException($exception->getMessage());
             }
         }
-
+        
         /**
          * Setter for the database driver
          *
@@ -92,7 +99,7 @@ namespace DatabaseFactory {
         {
             self::$driver = strtolower($driver);
         }
-
+        
         /**
          * Generate a new PDO connection
          *
@@ -104,7 +111,7 @@ namespace DatabaseFactory {
         {
             self::$connection = new PDO($string, self::$username, self::$password);
         }
-
+        
         public static function connection(): PDO
         {
             return self::$connection;
