9e20da54a98bbb5fc216b36a6a0bf7b1ac219a06
JNapolitanoIT <jnapolitanoit@gmail.com>
2023-01-18 08:45:47 -0800

    Update tests
    Used PHPCBF for the reformatting of tests to comply with PSR-12

--- tests/Feature/Helpers/StrTest.php
+++ tests/Feature/Helpers/StrTest.php
@@ -2,49 +2,34 @@
 
 namespace Tests\Feature\Helpers {
 
-    use DatabaseFactory\Helpers\Str;
+    use DatabaseFactory\Helpers;
     use Tests\TestCase;
 
     class StrTest extends TestCase
     {
-        public function testTransformStringToPluralForm(): void
-        {
-            $this->original = 'Car';
-            $this->modified = Str::plural($this->original);
-            $this->expected = 'Cars';
-
-            $this->assertEquals($this->modified, $this->expected);
-        }
-
+    
         public function testTransformStringToSingularForm(): void
         {
             $this->original = 'Cars';
-            $this->modified = Str::singular($this->original);
+            $this->modified = Helpers\Str::singular($this->original);
             $this->expected = 'Car';
+        
+            $this->assertEquals($this->modified, $this->expected);
+        }
+        
+        public function testTransformStringToPluralForm(): void
+        {
+            $this->original = 'Car';
+            $this->modified = Helpers\Str::plural($this->original);
+            $this->expected = 'Cars';
 
-            $this->assertEquals($this->modified, $this->expected);
-        }
-    
-        public function testConvertStringToLowercase(): void
-        {
-            $this->original = 'Cars';
-            $this->modified = Str::lower($this->original);
-            $this->expected = 'cars';
-            $this->assertEquals($this->modified, $this->expected);
-        }
-    
-        public function testConvertStringToUppercase(): void
-        {
-            $this->original = 'Cars';
-            $this->modified = Str::upper($this->original);
-            $this->expected = 'CARS';
             $this->assertEquals($this->modified, $this->expected);
         }
     
         public function testStripDoubleQuotesFromString(): void
         {
             $this->original = '"Cars"';
-            $this->modified = Str::stripQuotes($this->original);
+            $this->modified = Helpers\Str::stripQuotes($this->original);
             $this->expected = 'Cars';
             $this->assertEquals($this->modified, $this->expected);
         }
@@ -52,15 +37,31 @@ namespace Tests\Feature\Helpers {
         public function testStripSingleQuotesFromString(): void
         {
             $this->original = '\'Cars\'';
-            $this->modified = Str::stripQuotes($this->original);
+            $this->modified = Helpers\Str::stripQuotes($this->original);
             $this->expected = 'Cars';
+            $this->assertEquals($this->modified, $this->expected);
+        }
+    
+        public function testConvertStringToLowercase(): void
+        {
+            $this->original = 'Cars';
+            $this->modified = Helpers\Str::lower($this->original);
+            $this->expected = 'cars';
+            $this->assertEquals($this->modified, $this->expected);
+        }
+    
+        public function testConvertStringToUppercase(): void
+        {
+            $this->original = 'Cars';
+            $this->modified = Helpers\Str::upper($this->original);
+            $this->expected = 'CARS';
             $this->assertEquals($this->modified, $this->expected);
         }
     
         public function testConvertStringToSlug(): void
         {
             $this->original = 'Some cars are very fast';
-            $this->modified = Str::slug($this->original);
+            $this->modified = Helpers\Str::slug($this->original);
             $this->expected = 'some-cars-are-very-fast';
             $this->assertEquals($this->modified, $this->expected);
         }
