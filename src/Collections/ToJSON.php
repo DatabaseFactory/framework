442bff58850d48ede2ce49c31c08f2c45942fed9
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2023-02-15 08:18:52 -0800

    Update ToJSON.php

--- src/Collections/ToJSON.php
+++ src/Collections/ToJSON.php
@@ -2,13 +2,32 @@
 
 namespace DatabaseFactory\Collections {
 
+    /**
+     * Takes a collection of data and provides the ability to access 
+     * it as JSON
+     *
+     * @package DatabaseFactory\Collections
+     * @author  Jason Napolitano
+     *
+     * @version 1.0.0
+     * @since   1.0.0
+     * @license MIT <https://mit-license.org>
+     */
     readonly class ToJSON implements \JsonSerializable
     {
+        /**
+         * Constructor
+         */
         public function __construct(private array $collection = [])
         {
             // ...
         }
 
+        /**
+         * Specify data which should be serialized to JSON
+         *
+         * @link https://www.php.net/manual/en/jsonserializable.jsonserialize.php
+         */
         public function jsonSerialize(): array
         {
             return $this->collection;
