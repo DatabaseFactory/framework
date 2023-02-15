21899fdc36b280b52ad8ec927218d7641c42da48
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2023-02-15 08:17:08 -0800

    Update ToArray.php

--- src/Collections/ToArray.php
+++ src/Collections/ToArray.php
@@ -2,18 +2,40 @@
 
 namespace DatabaseFactory\Collections {
 
+    /**
+     * Takes a collection of data and provides the ability to access 
+     * objects as arrays
+     *
+     * @package DatabaseFactory\Collections
+     * @author  Jason Napolitano
+     *
+     * @version 1.0.0
+     * @since   1.0.0
+     * @license MIT <https://mit-license.org>
+     */
     class ToArray implements \ArrayAccess
     {
+        /**
+         * Constructor
+         */
         public function __construct(private array $collection)
         {
             // ...
         }
 
+        /**
+         * Returns the raw collection of data
+         */
         public function collection(): array
         {
             return $this->collection;
         }
 
+        /**
+         * Assigns a value to the specified offset
+         *
+         * @link https://www.php.net/manual/en/arrayaccess.offsetset.php
+         */
         public function offsetSet($offset, $value): void
         {
             !is_null($offset)
@@ -21,16 +43,31 @@ namespace DatabaseFactory\Collections {
                 : $this->collection[] = $value;
         }
 
+        /**
+         * Checks to see if an offset exists
+         *
+         * @link https://www.php.net/manual/en/arrayaccess.offsetexists.php
+         */
         public function offsetExists($offset): bool
         {
             return isset($this->collection[$offset]);
         }
 
+        /**
+         * Unset an offset
+         *
+         * @link https://www.php.net/manual/en/arrayaccess.offsetunset.php
+         */
         public function offsetUnset($offset): void
         {
             unset($this->collection[$offset]);
         }
 
+        /**
+         * Retrieves an offset
+         *
+         * @link https://www.php.net/manual/en/arrayaccess.offsetget.php
+         */
         public function offsetGet($offset): ?int
         {
             return $this->collection[$offset] ?? null;
