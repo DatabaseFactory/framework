a580c43a2cd677d1ec599f4d6500ebb5065c42e3
jason-napolitano <jnapolitanoit@gmail.com>
2023-09-02 09:57:30 -0700

    Small Updates

--- src/Entity.php
+++ src/Entity.php
@@ -2,10 +2,8 @@
 
 namespace DatabaseFactory {
 
-    use ReflectionClass;
-    use ReflectionProperty;
-	use DatabaseFactory\ORM;
 	use DatabaseFactory\Facades;
+	use DatabaseFactory\ORM;
 
     /**
      * The base entity class
@@ -21,58 +19,15 @@ namespace DatabaseFactory {
     class Entity
     {
         // ORM Plugins
-        use ORM\HasTable;
-        use ORM\HasQuery;
-
-        /**
-         * ID of a record for updating
-         *
-         * @var int|null $id
-         */
-        private ?int $id;
+	    use ORM\HasQuery;
+	    use ORM\HasTable;
+	    use ORM\HasSave;
 
         /**
          * Constructor
          *
          * @param int|null $id Pass an ID to update a record
          */
-        public function __construct(int $id = null)
-        {
-            $this->id = $id;
-        }
-
-        /**
-         * Save a record
-         *
-         * @return bool|int
-         */
-        public function save(): bool|int
-        {
-            // SET props
-            $propsToImplode = [];
-
-            // default ID
-            $this->id ??= 0;
-
-            // generate SET props
-            foreach ((new ReflectionClass($this))->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
-                $propertyName = trim($property->getName(), $property->getModifiers());
-                $propsToImplode[] = '`' . $propertyName . '` = "' . $this->{$propertyName} . '"';
-            }
-
-            // generate the SET clause by gluing all key value
-            // pairs together
-            $setClause = implode(',', $propsToImplode);
-
-            // are we updating an existing record, or creating a new one?
-            if ($this->id > 0) {
-                $sqlQuery = 'UPDATE `' . static::table() . '` SET ' . $setClause . ' WHERE id = ' . $this->id;
-            } else {
-                $sqlQuery = 'INSERT ' . 'INTO ' . static::table() . ' SET ' . $setClause . ', id = ' . $this->id;
-            }
-
-            // finally, let's execute the query
-            return Facades\DB::connection()->exec($sqlQuery);
-        }
+        public function __construct(private ?int $id = null) {}
     }
 }
